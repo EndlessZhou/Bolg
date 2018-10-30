da9e6d5f25cc2c54be2f64912c640e1428ae9c79
zhouchong <zhouchong.wh@bytedance.com>
2018-10-31 00:36:43 +0800

    ok

---
+++ blogproject/urls.py
@@ -0,0 +1,33 @@
+"""blogproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+import admin
+from blog.feeds import AllPostsRssFeed
+from django.shortcuts import render
+
+admin.site.site_header = '博客管理'
+admin.site.site_title = 'black&white'
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
+    url(r'', include('comments.urls')),
+    url(r'^all/rss/$', AllPostsRssFeed(), name='rss'),
+    url(r'^search/', include('haystack.urls')),
+
+]
+
+
+
